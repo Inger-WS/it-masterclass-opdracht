a036e3b30541f80e2876676e8bf99c25f1a74c46
UNICA\i.smit <i.smit@workingspirit.nl>
2024-11-29 13:15:18 +0100

    Added recieve function to server

--- Nodejs_server/server.js
+++ Nodejs_server/server.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hello, World!');
+});
+
+// add backend endpoint
+app.post('/submit', (req, res) => {
+    const { name } = req.body;
+    console.log(`Received name: ${ name }`);
+    res.json({ message: `Hello, ${ name }!` });
 });
 
 app.listen(port, () => {
