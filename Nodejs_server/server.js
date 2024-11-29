b9776c9fb18b7da3cf20b5ef0cb28b8438ae6ae9
UNICA\i.smit <i.smit@workingspirit.nl>
2024-11-29 14:06:24 +0100

    Added cors

--- Nodejs_server/server.js
+++ Nodejs_server/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
+const corsOptions = {
+    origin: '127.0.0.1:3000', // Specifiek domein toestaan
+    methods: ['GET', 'POST'], // Specifieke HTTP-methoden toestaan
+    allowedHeaders: ['Content-Type', 'Authorization'], // Specifieke headers toestaan
+};
+app.use(cors());
+    
 app.use(express.json());
 
 app.get('/', (req, res) => {
