ffaf1efe64be3b9a4ba44d931de7b58518fa4cb2
UNICA\i.smit <i.smit@workingspirit.nl>
2024-11-29 16:22:08 +0100

    Added database connection

--- Nodejs_server/database.js
+++ Nodejs_server/database.js
@@ -1,17 +1,26 @@
-const { MongoClient } = require('mongodb');
-const uri = "your_connection_string_here"; // Replace with your connection string
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://ingersmit12:<XkywOxHmZBJGUmzi>@cluster0.cvba2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
 
 async function run() {
     try {
+        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        console.log("Connected to MongoDB Atlas!");
-        const database = client.db('mydb');
-        const collection = database.collection('users');
-        // Perform database operations
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
+        // Ensures that the client will close when you finish/error
         await client.close();
     }
 }
-
 run().catch(console.dir);
